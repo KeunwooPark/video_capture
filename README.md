fbad3a9725f734e0348f9ea733ce5c5678eba5c7
keunwoo <avolic@kaist.ac.kr>
2018-07-18 16:03:16 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# video_capture
+# Video Capture
 Simple python script for capturing video.
+It uses OpenCV.
+
+## How to use
+1. Do not record, but show live images
+<pre><code>$ python record.py
+</code></pre>
+
+1. Open camera setting panel.
+<pre><code>$ python record.py --setting
+</code></pre>
+
+1. Save images to directory. All the streaming images will be saved automatically
+<pre><code>$ python record.py --save dir/to/save --mode a
+</code></pre>
+
+1. Save images to directory. All the streaming images will be saved manually. If you press 'space' button, it will save an image.
+<pre><code>$ python record.py --save dir/to/save --mode m
+</code></pre>
+
+## Help
+<pre><code>$ python record.py --help
+</code></pre>
---
+++ record.py
@@ -0,0 +1,44 @@
+import cv2
+import os
+import argparse
+
+parser = argparse.ArgumentParser(description='Simple video capture script.')
+parser.add_argument('--save', nargs = '?', help='Directory path to save image. If not save mode, it is ignored.')
+parser.add_argument('--setting', action='store_true', help='Open camera setting pannel. Other flags are all ignored.')
+parser.add_argument('--mode', choices=['a', 'm'], default='m', help='capture mode. "a" for automatic capturing, "m" for manual capturing')
+
+args = parser.parse_args()
+#print(parser)
+
+cap = cv2.VideoCapture(0)
+if args.setting:
+    cap.set(cv2.CAP_PROP_SETTINGS, 0.0)
+
+def get_file_name(args, id):
+    return "{}/{}.png".format(args.save, id)
+
+if args.save and not os.path.exists(args.save):
+    os.makedirs(args.save)
+
+if args.mode == 'a':
+    is_auto = True
+else:
+    is_auto = False
+
+frame_id = 0
+while True:
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
+
+    img_file_name = get_file_name(args, frame_id)
+
+    input_key = cv2.waitKey(1) & 0xFF
+    if input_key == ord('q'):
+        break
+
+    if args.setting or args.save is None:
+        continue
+
+    if is_auto or (input_key == ord(' ')):
+        cv2.imwrite(img_file_name, frame)
+        frame_id += 1
